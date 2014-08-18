f8f4c97692501f5d19a8724fbcff1b1c976f5c07
Jeff Stringer & Blake Mason <student@epicodus.com>
2014-08-18 16:25:10 -0700

    add phone class

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
   validates :name, :presence => true
   #validates :phone, numericality: true, unless: "contact.name.nil?"
-
+  has_many :phones
 end
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -2,6 +2,4 @@ require 'spec_helper'
 
 describe Contact do
   it { should validate_presence_of :name }
-  it { should validate_numericality_of :phone }
-
 end
